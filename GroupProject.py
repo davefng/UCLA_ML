0b712b3202af10f19c573fb80061bb22f36c4e64
davefng <davefng@users.noreply.github.com>
2020-08-01 11:44:03 -0700

    Create GroupProject.py
    Main Python File

---
+++ GroupProject.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug  1 10:33:31 2020
+
+@author: David Fong, Rachel ChengJie Ren, Cesar Martinez, Bruce Bothwell
+""" 
+
+import os
+import pandas as pd
+
+os.getcwd()
+    
+myFile = pd.read_csv('DataFiles/Police_Spending.csv',low_memory=False)
+myFile.corr()
+
+colToDrop = ['DETAILED ITEM DESCRIPTION','VENDOR NUM']
+myFile = myFile.drop(colToDrop,axis=1)
+len(myFile.columns)
+
+
+
+
+
+
+
